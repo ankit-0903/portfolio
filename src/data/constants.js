9b41d80d31245f92aef24d1e54087fc4d977e947
ankit-0903 <1by22cs022@bmsit.in>
2024-04-22 22:40:38 +0530

    first commit

---
+++ src/data/constants.js
@@ -0,0 +1,156 @@
+export const Bio = {
+    name: "Ankit Kumar Gupta",
+    roles: [
+      "Front-end Developer",
+      "UI/UX Designer",
+      "Programmer",
+    ],
+    description:
+      "As a CSE student, I'm constantly fascinated by the ever-evolving world of technology. With a passion for learning, I eagerly dive into new challenges, driven by the desire to understand and innovate in this dynamic field.",
+    github: "https://github.com/ankit-0903",
+    /*resume:
+      "https://drive.google.com/file/d/1ffZrcMcn8UatXGIaautbbqpV7ADNaETA/view?usp=sharing",*/
+    linkedin: "https://www.linkedin.com/in/ankit-kumar-gupta-a39688234/",
+    insta: "https://www.instagram.com/ankit.gupta1218/",
+  };
+  
+  export const skills = [
+    {
+      title: "Frontend",
+      skills: [
+        {
+          name: "HTML",
+          image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+        },
+        {
+          name: "CSS",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+        },
+        {
+          name: "JavaScript",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+        },
+        {
+          name: "React Js",
+          image:
+            "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+        },
+        {
+          name: "Bootstrap",
+          image:
+            "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+        },
+      ],
+    },
+    {
+      title: "Others",
+      skills: [
+        {
+          name: "Git",
+          image:
+            "https://camo.githubusercontent.com/fbfcb9e3dc648adc93bef37c718db16c52f617ad055a26de6dc3c21865c3321d/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6769742d73636d2f6769742d73636d2d69636f6e2e737667",
+        },
+        {
+          name: "GitHub",
+          image:
+            "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+        },
+        {
+          name: "VS Code",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+        },
+        {
+          name: "Figma",
+          image:
+            "https://camo.githubusercontent.com/ed93c2b000a76ceaad1503e7eb9356591b885227e82a36a005b9d3498b303ba5/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6669676d612f6669676d612d69636f6e2e737667",
+        },
+      ],
+    },
+  ];
+  
+  export const experiences = [
+    {
+      id: 0,
+      //img: "https://media.licdn.com/dms/image/C560BAQFQb9_Lb3bZug/company-logo_200_200/0/1644414857251?e=1693440000&v=beta&t=7WqhIiUCg_0otFDwWn-O_SP-Z4Ftoe4be1WlnjYoESc",
+      role: "Frontend Intern",
+      company: "Arniko College",
+      date: "2020 - 2021",
+      desc: "Worked on the frontend part of the college website using HTML, CSS, and basics of ReactJS.",
+      skills: [
+        "HTML",
+        "CSS",
+        "JavaScript",
+        "ReactJS"
+      ],
+    },
+  ];
+  
+  export const education = [
+    {
+      id: 0,
+      //img: "https://media.licdn.com/dms/image/C560BAQE4UC9Z_k3c7A/company-logo_200_200/0/1579422507409?e=1692230400&v=beta&t=HJssPsrwwkAJAsvFiVIYPdM8GzUuuSK5YlATKgLx3Bc",
+      school: "BMS Institute of Technology and Management,Bangalore",
+      date: "Nov 2022 - Present",
+      grade: "89.05 CGPA",
+      desc: "I am currently pursuing a Bachelor's degree in Computer Science and Engineering at BMS Institute of Technology and Management,Bangalore. I have completed 2nd semesters and have a CGPA of 9.05.",
+      degree: "Bachelor of Technology - BTech, Computer Science and Engineering",
+    },
+    {
+      id: 1,
+      //img: "https://methodistschool-dankuni.webs.com/NEW%20BADGE.jpg",
+      school: "Arniko College, Biratnagar, Nepal",
+      date: "2019 - 2021",
+      grade: "Grade-A",
+      desc: "I completed my class 12 high school education at Arniko College, where I studied Science with Computer Science.",
+      degree: "NEB(XII), Science with Computer",
+    },
+    {
+      id: 2,
+      //img: "https://methodistschool-dankuni.webs.com/NEW%20BADGE.jpg",
+      school: "Sagarmatha Secondary Boarding School, Biratnagar, Nepal",
+      //date: "Apr 2017 - Apr 2019",
+      grade: "87%",
+      desc: "I completed my class 10 education at Sagarmatha Secondary Boarding School.",
+      degree: "NEB(X),Science",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 0,
+      title: "Snake Game Using C++",
+      description:
+        "Developed a terminal based Snake Game using the concept of DSA, Array, Linked list. And other concepts of C++",
+      image:
+        "https://user-images.githubusercontent.com/64485885/234602896-a1bd8bcc-b72b-4821-83d6-8ad885bf435e.png",
+      tags: ["C++", "DSA"]
+    },
+    {
+      id: 1,
+      title: "Website of Govt. Land Information System(GLlS)",
+      description:
+        "In order to help urban planners make decisions about land use and Infrastructure development, we developed a website prototype that acts as a platform for both users and admins.",
+      image:
+        "https://user-images.githubusercontent.com/64485885/234916413-96296f13-fe4b-4cc4-b215-e72bd7c27928.png",
+      tags: [
+        "HTML",
+        "CSS",
+        "Javascript",
+        "React Js",
+        "MongoDb",
+        "Node Js",
+      ],
+    },
+  ];
+  
+  export const TimeLineData = [
+    { year: 2017, text: "Started my journey" },
+    { year: 2018, text: "Worked as a freelance developer" },
+    { year: 2019, text: "Founded JavaScript Mastery" },
+    { year: 2020, text: "Shared my projects with the world" },
+    { year: 2021, text: "Started my own platform" },
+  ];
+  
